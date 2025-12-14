895b4430fc60991f19e49b29fdb98ff26ce2e856
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-14 15:27:36 +0000

    sd

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 """
-Bu modul bir array-i zoom edən funksiyanı ehtiva edir.
+Bu modul bir tuple-i zoom edən funksiyanı ehtiva edir.
 """
 
-from typing import List
+from typing import List, Tuple
 
-
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    Verilmiş list-i factor qədər zoom edir.
+    Verilmiş tuple-i factor qədər zoom edir.
     Hər elementi factor dəfə təkrarlayır.
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for _ in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)  # factor mütləq int olmalıdır
+zoom_3x = zoom_array(array, 3)
