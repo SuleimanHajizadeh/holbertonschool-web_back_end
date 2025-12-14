1f39ab6057645c226a9339c1e0fd3196fb3647b7
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-14 17:17:50 +0400

    sd

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Bu modul float tipində iki ədədin cəmini hesablayan funksiyanı ehtiva edir."""
+"""Modul float tipində iki ədədin cəmini hesablayan funksiyanı ehtiva edir."""
+
 
 def add(a: float, b: float) -> float:
     """İki float tipində ədədi toplayır və nəticəni float tipində qaytarır."""
