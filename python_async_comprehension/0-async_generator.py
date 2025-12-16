615fc4a3eacdaa3d07a75f8888aaa7650b0d4cbc
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-17 00:29:26 +0400

    sd

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Async generator that yields random numbers."""
+"""Async generator module."""
 
 import asyncio
 import random
 
 
 async def async_generator():
-    """Async generator that yields random numbers."""
+    """Async generator coroutine."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
