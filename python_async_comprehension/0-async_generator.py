74df65708520ea2d808bea08eee87b5a44cdc8f8
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-17 00:34:03 +0400

    sd

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async generator module."""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """Async generator coroutine."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
