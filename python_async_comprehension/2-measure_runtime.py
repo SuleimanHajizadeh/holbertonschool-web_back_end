a5fd79a88ef5140232ed8bc17344b0cd2a2e0b08
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-16 20:28:43 +0400

    sd

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,34 +1,17 @@
 #!/usr/bin/env python3
-"""
-measure_runtime.py
 
-Bu modul async_comprehension funksiyasını 4 paralel iş olaraq icra edir
-və ümumi run-time-i ölçür.
-"""
+""" Async Comprehensions """
 
-import asyncio
-import time
-import importlib.util
-import sys
-from pathlib import Path
+from asyncio import gather
+from time import time
 
-# Dinamik import 1-async_comprehension.py
-file_path = Path(__file__).parent / "1-async_comprehension.py"
-spec = importlib.util.spec_from_file_location("async_comprehension", file_path)
-async_module = importlib.util.module_from_spec(spec) # type: ignore
-sys.modules["async_comprehension"] = async_module
-spec.loader.exec_module(async_module) # type: ignore
-
-async_comprehension = async_module.async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    start: float = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    end: float = time.time()
-    return end - start
+    """ Run time for four parallel comprehensions """
+    start = time()
+    tasks = [async_comprehension() for i in range(4)]
+    await gather(*tasks)
+    end = time()
+    return (end - start)
