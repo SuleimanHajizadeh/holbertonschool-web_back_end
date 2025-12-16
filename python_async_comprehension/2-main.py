216cb66ded3706827686498b294e6a6a4927e5aa
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-16 20:22:54 +0400

    sd

--- python_async_comprehension/2-main.py
+++ python_async_comprehension/2-main.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
+
 import asyncio
-import importlib.util
-import sys
-from pathlib import Path
 
-# Fayl yolunu göstəririk
-file_path = Path(__file__).parent / "2-measure_runtime.py"
-spec = importlib.util.spec_from_file_location("measure_runtime", file_path)
-module = importlib.util.module_from_spec(spec)
-sys.modules["measure_runtime"] = module
-spec.loader.exec_module(module)
 
-measure_runtime = module.measure_runtime
+measure_runtime = __import__('2-measure_runtime').measure_runtime
+
 
 async def main():
-    total_time = await measure_runtime()
-    print(total_time)
+    return await(measure_runtime())
 
-asyncio.run(main())
+print(
+    asyncio.run(main())
+)
