6ac8903aa1c943044c2d9ad60c9ae0a78f2bc4ca
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-17 00:19:17 +0400

    sd

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 """
-Async Generator
+0-async_generator.py
+
+Bu modul 1 saniyəlik gecikmə ilə 10 dəfə təsadüfi ədəd
+yield edən asinxron generator təqdim edir.
 """
+
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
-    """Yield a random number between 0 and 10"""
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Hər iterasiyada 1 saniyə gözləyərək 0 ilə 10 arasında
+    təsadüfi float ədəd yield edən asinxron generator funksiyasıdır.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
