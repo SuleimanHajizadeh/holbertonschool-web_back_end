7e770a1380478eb11f539735b3ea110aff349666
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-16 21:02:41 +0400

    sd

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Async Generator"""
-import random
+"""
+Async Generator
+"""
 import asyncio
+import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """async_generator function"""
-
+    """Yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,13 +2,16 @@
 """Run time for four parallel comprehensions"""
 import asyncio
 import time
+
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """measure_runtime function"""
-    start = time.time()
-    await asyncio.gather(async_comprehension(), async_comprehension(),
-                         async_comprehension(), async_comprehension())
-    end = time.time()
-    return end - start
+    """Measure the runtime"""
+    start: float = time.perf_counter()
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
+    end: float = time.perf_counter() - start
+    return end
