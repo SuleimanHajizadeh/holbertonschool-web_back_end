ce4d991698c9b82c9d54025b056c6041c15f36e1
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-16 20:02:11 +0400

    sd

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,30 +2,30 @@
 """
 2-measure_runtime.py
 
-Bu modul async_comprehension funksiyasını 4 paralel iş olaraq icra edir
-və ümumi run-time-i ölçür.
+Bu modul 1-async_comprehension.py faylını dinamik import edir
+və async_comprehension funksiyasını 4 paralel iş olaraq icra edir.
 """
 
 import asyncio
 import time
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+import importlib
+
+# Dinamik import
+module = importlib.import_module("1-async_comprehension")
+async_comprehension = module.async_comprehension
 
 
 async def measure_runtime() -> float:
     """
-    async_comprehension funksiyasını 4 paralel iş olaraq icra edir
-    və ümumi run-time-i ölçərək float tipində qaytarır.
-    
-    Qeyd: Hər async_comprehension 10 random ədəd yaradır və
-    hər biri 1 saniyə gecikmə ilə işləyir. 4 paralel iş icra
-    olunduğuna görə ümumi vaxt təxminən 10 saniyə olur.
+    1-async_comprehension modulundan async_comprehension funksiyasını
+    4 paralel iş olaraq icra edir və run-time ölçür.
     """
     start = time.time()
     await asyncio.gather(
-        async_comprehension(), # type: ignore
-        async_comprehension(), # type: ignore
-        async_comprehension(), # type: ignore
-        async_comprehension() # type: ignore
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
     )
     end = time.time()
     return end - start
