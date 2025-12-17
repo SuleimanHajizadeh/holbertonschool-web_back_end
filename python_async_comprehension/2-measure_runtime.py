6d8c922126591c6be828ebcf798bd575d4de7c26
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-17 15:37:21 +0400

    sd

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,11 +3,13 @@
 This module contains a coroutine that measures the runtime of
 parallel asynchronous comprehensions.
 """
+
 import asyncio
 import time
 
-async_comprehension = \
+async_comprehension = (
     __import__('1-async_comprehension').async_comprehension
+)
 
 
 async def measure_runtime() -> float:
