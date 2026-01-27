0473b61e0ef7e4a038bfd771a24407bbc8f9f0f0
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2026-01-27 18:51:40 +0000

    sd

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,3 +1,4 @@
+// 0-constants.js
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -10,7 +11,6 @@ export function getLast() {
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
-
   return combination;
 }
 
