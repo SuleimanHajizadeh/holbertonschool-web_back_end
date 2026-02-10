eb0ede91f02457c60d574ce3cc7bccdea8587f4f
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2026-02-10 13:54:24 +0000

    sd

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
   return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
