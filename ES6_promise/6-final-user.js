e4c75460c3c7c2d36f4c696d846a4c190fdb7b0a
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2026-02-17 09:55:11 +0000

    sd

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+
+  return Promise.allSettled(promises)
+    .then(results => results.map(result => {
+      if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+      } else {
+        return { status: 'rejected', value: result.reason };
+      }
+    }));
+}
+
