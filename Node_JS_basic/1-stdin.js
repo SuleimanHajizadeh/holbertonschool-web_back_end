2b0ef170ce7d9676408d752c6d4a6dac925151ad
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2026-02-24 15:18:28 +0000

    sd

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (input) => {
-  const name = input.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  const name = input.replace(/\r?\n|\r/g, '');
+
+  if (name.length > 0) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
 });
 
 process.stdin.on('end', () => {
